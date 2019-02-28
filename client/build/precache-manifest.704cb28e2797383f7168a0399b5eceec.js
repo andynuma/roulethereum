40ce39c340b0404bafd4e7be3de0a9312a8f0d46
andynuma <c126001a@comwork.c.oka-pu.ac.jp>
2019-02-28 18:11:36 +0900

    add some components

---
+++ client/build/precache-manifest.704cb28e2797383f7168a0399b5eceec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "358ad9c3b30f1f774f93",
+    "url": "/roulethereum/static/js/runtime~main.358ad9c3.js"
+  },
+  {
+    "revision": "cb396ec33a4aa8ae2020",
+    "url": "/roulethereum/static/js/main.cb396ec3.chunk.js"
+  },
+  {
+    "revision": "8ce440b5f967c9bcd3e8",
+    "url": "/roulethereum/static/js/2.8ce440b5.chunk.js"
+  },
+  {
+    "revision": "cb396ec33a4aa8ae2020",
+    "url": "/roulethereum/static/css/main.2f171d10.chunk.css"
+  },
+  {
+    "revision": "555e173a6f2ba0ff551aed0343370e50",
+    "url": "/roulethereum/index.html"
+  }
+];
